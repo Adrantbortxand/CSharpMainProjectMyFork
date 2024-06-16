eb895b74e10a364c419064b66b2ef69c21399a34
Vladimir Chuchkalov <v.chuchkalov@yandex.ru>
2024-06-16 13:32:18 +0300

    refactor: remove unused imports

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Model;
 using Model.Runtime.Projectiles;
 using UnityEngine;
 using Utilities;
-using static UnityEngine.GraphicsBuffer;
 
 namespace UnitBrains.Player
 {
